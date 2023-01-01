e47daaa2d9228a97f852c33960d25a55601dfcf7
Aashirbad Sabat <aum.aashirbad@gmail.com>
2023-01-01 09:48:19 +0530

    Initial Commit

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style/landing.css";
+
+const LandingPage = () => {
+  return <div>LandingPage</div>;
+};
+
+export default LandingPage;
---
+++ src/Components/Patient.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style/patient.css";
+
+const Patient = () => {
+  return <div>Patient</div>;
+};
+
+export default Patient;
